0954e097fa226b4c8d4a65da12eaf4e4545ca3d4
dudwo123wjs <55525101+dudwo123wjs@users.noreply.github.com>
2020-01-10 14:07:28 +0900

    asd
    s

--- src/main/java/com/iu/b1/qna/QnaRepository.java
+++ src/main/java/com/iu/b1/qna/QnaRepository.java
@@ -13,24 +13,19 @@ import org.springframework.data.repository.query.Param;
 public interface QnaRepository extends JpaRepository<QnaVO, Integer>{
 
 	
-	//Page<QnaVO> findByNumGreaterThan(int num, Pageable pageable)throws Exception;
-	//ref desc / step asc
+	//List <QnaVO> findByNumGreaterThan(int num, Pageable pageable) throws Exception;
 	
-	@Query(value = "select q from QnaVO q order by q.ref desc, q.step asc")
-	List<QnaVO> findQnas()throws Exception;
+	@Query(value = "SELECT q FROM QnaVO q order by q.ref desc, q.step asc")
+	List <QnaVO> findQnas()throws Exception;
 	
-//	@Query("select q from QnaVO q where num = ?1")
-//	QnaVO findQnaVO(int n)throws Exception;
+	@Query(value = "select q from QnaVO q where num= :num", nativeQuery = true)
+	QnaVO findQnaVO(@Param("num")int num)throws Exception;
 	
-	@Query(value = "select q from QnaVO q where num = :num ", nativeQuery = true)
-	QnaVO findQnaVO(@Param("num") int num)throws Exception;
-
-	@Modifying(clearAutomatically = true)
-	@Query("update QnaVO q set title = :title, contents = :contents where num = :num")
-	void qnaUpdate(@Param("title") String title,@Param("contents") String contents, @Param("num") int num)throws Exception;
+	@Modifying
+	@Query("update QnaVO q set q.title= ?1, q.contents= ?2 where q.num= ?3")
+	void qnaUpdate(String title, String contents, int num)throws Exception;
 	
-	@Query("select q.writer, q.contents from QnaVO q where num 1 =?1")
+	@Query("select q.writer, q.contents from QnaVO q where num=?1")
 	List<Object[]> qnaSelect(int num)throws Exception;
-	
 	
 } 
